326deebfc5eb496af674d7d281eb2e2f926e0993
malnaqeeb <mosleh.alnaqeeb@gmail.com>
2022-06-18 20:37:49 +0200

    on push

--- infra/terraform/dynamodb.tf
+++ infra/terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "auction" {
-  name         = "tf-${var.stage}-auctions"
+  name         = "tf-${var.stage}-auctions1"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
